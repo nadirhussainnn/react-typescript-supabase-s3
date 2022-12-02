5c6b1e24e441bb8b75deec28d32182472daf3d19
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-12-02 20:10:35 +0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Link, Routes, Route } from 'react-router-dom'
 import './app.scss'
 import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
 
 type Props = {}
 
@@ -11,9 +9,7 @@ export default function App({ }: Props) {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/register" element={<Register />}></Route>
-
+     
     </Routes>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,21 +4,27 @@ type Props = {}
 
 export default function Home({ }: Props) {
     return (
+
         <div className='container'>
+            <h1 className='heading'>Supabase - CRUD and Github authentication</h1>
+            <form className='form'>
+                <div>
+                    <input type={"text"} placeholder="username" className='text'></input>
+                </div>
+                <div>
+                    <input type={"password"} placeholder="Password" className='text'></input>
+                </div>
+
+            </form>
             <div className='sub-container'>
-            <div>
-            <Link to="/login">
                 <button className="btn">Login</button>
-            </Link>
+                <button className="btn">Register</button>
             </div>
 
-            <div>
-            <Link to="/register">
-                <button className="btn">Register</button>
-            </Link>
+            <div className='sub-container'>
+                <button className="btn">Login With Github</button>
+                <button className="btn">Register With Email</button>
             </div>
-            </div>
-          
 
         </div>
     )
